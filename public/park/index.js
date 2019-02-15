11a0842ee7d4f71386881696891a31119f692701
decolo <decolo.com>
2019-02-15 11:21:11 +0800

    停车场bug

--- public/park/index.js
+++ public/park/index.js
@@ -19,7 +19,7 @@ var QueryPark = (function() {
     if (!ticket) {
       alert('url中缺少ticket');
       return;
-    }
+    };
 
     var timestamp = Date.parse(new Date());
     var uuid = setUUID();
@@ -48,8 +48,6 @@ var QueryPark = (function() {
           self.userCenter.usercode = data.login_name;
           self.userCenter.username = data.name;
           self.userCenter.mobile = data.tel_number;
-          // self.userCenter.userLoginName = data.login_name;
-          // console.log(res);
           self.fetchUserID();
         } else {
           alert(res.code);
@@ -61,36 +59,26 @@ var QueryPark = (function() {
     };
     $.ajax(settings);
   };
-  
-  _QueryPark.prototype.requestTime = function(date) {
-    var requestTimeString = '';
-    requestTimeString += date.getFullYear();
-    requestTimeString += date.getMonth() + 1;
-    requestTimeString += date.getDate();
-    requestTimeString += date.getHours();
-    requestTimeString += date.getMinutes();
-    requestTimeString += date.getSeconds();
-    return requestTimeString;
-  };
 
   _QueryPark.prototype.fetchUserID = function() {
     var self = this;
+
     var visitTime = Date.parse(new Date());
+    var data = {
+      method: 'getClerkCode',
+      usercode: this.userCenter.usercode,
+      visitTime: visitTime,
+      key: md5('getClerkCode_' + visitTime + '_^#Erp,.[-]')
+    };
   
     var settings = {
-      // http://erp.8531.cn/service/MobilePaylipServlet
       url: '/service/MobilePaylipServlet',
       type: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       dataType: 'json',
-      data: {
-        method: 'getClerkCode',
-        usercode: this.userCenter.usercode,
-        visitTime: visitTime,
-        key: md5('getClerkCode_' + visitTime + '_^#Erp,.[-]')
-      },
+      data: data,
       success: function(res) {
         if (res.flag === 0) {
           self.userCenter.userID = res.clerkCode;
@@ -109,30 +97,28 @@ var QueryPark = (function() {
   
   _QueryPark.prototype.fetchParkInfo = function() {
     var self = this;
+
     var date = new Date();
     var transactionID = Date.parse(date).toString();
-    
     var busiInfo = {
       licensePlate: '',
       custCode: this.userCenter.userID,
       custName: this.userCenter.username,
       phone: this.userCenter.mobile
     };
-
     var busiInfoString = JSON.stringify({
       busiInfo: busiInfo
     });
-
     var md5String = busiInfoString.substring(1, busiInfoString.length - 1);
     var sign = md5(transactionID + md5String.length + md5String + 'MD5Key');
   
-    var params = {
+    var data = {
       busiInfo: busiInfo,
       reqPubInfo: {
         systemNo: '01',
         method: 'queryCustomerBalance',
         transactionID: transactionID.toString(),
-        requestTime: this.requestTime(date),
+        requestTime: requestTime(date),
         version: '1.0',
         sign: sign
       }
@@ -146,12 +132,10 @@ var QueryPark = (function() {
         'Content-Type': 'application/json',
         'X-Requested-With': ''
       },
-      data: JSON.stringify(params),
+      data: JSON.stringify(data),
       dataType: 'json',
       success: function(res) {
-        console.log(res)
         var result = res.resPubInfo.result;
-
         if (result.code === '0000') {
           self.render(res);  
         } else {
@@ -182,7 +166,7 @@ var QueryPark = (function() {
     $('.name').text(this.userCenter.username);
     $('.data.worker-id').text(this.userCenter.userID);
     $('.data.car-id').text(busiInfo.customerInfo.licensePlate);
-    $('.data.time-left').text(busiInfo.customerInfo.balanceInfos[0].balanceUnit + '小时');
+    $('.data.time-left').text(Math.floor(busiInfo.customerInfo.balanceInfos[0].accountBalance / 60) + '小时');
   };
 
   _QueryPark.prototype.renderNoPark = function() {
@@ -223,41 +207,13 @@ function setUUID() {
   return uuid;
 };
 
-// _QueryPark.prototype.fetchUserInfo = function() {
-//     var self = this;
-//     var settings = {
-//       url: '/8531ClientService/InfoWebService/EIPWebService.asmx/GetUserInfoByUserID',
-//       type: 'POST',
-//       headers: {
-//         // default: application/x-www-form-urlencoded
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//       },
-//       dataType: 'xml',
-//       data: {
-//         'UserID': this.userCenter.usercode
-//       },
-//       success: function(res) {
-//         var data;
-//         if (typeof res === 'string') {
-//           data = JSON.parse(res);
-//         } else {
-//           data = JSON.parse(res.children[0].innerHTML);
-//         };
-//         console.log(data);
-//         if (data.ErrCode === '正常') {
-//           var userInfo = data.Rows[0];
-//           self.userCenter.username = userInfo.UserName;
-//           self.userCenter.userLoginName = userInfo.userLoginName;
-//           console.log(userInfo.username, userInfo.userLoginName);
-//           self.userCenter.mobile = userInfo.Mobile;
-          
-//         } else {
-//           alert(data.ErrCode);
-//         };
-//       },
-//       error: function() {
-//         alert('获取用户信息出错');
-//       }
-//     };
-//     $.ajax(settings);
-//   };
+function requestTime(date) {
+  var requestTimeString = '';
+  requestTimeString += date.getFullYear();
+  requestTimeString += date.getMonth() + 1;
+  requestTimeString += date.getDate();
+  requestTimeString += date.getHours();
+  requestTimeString += date.getMinutes();
+  requestTimeString += date.getSeconds();
+  return requestTimeString;
+};
